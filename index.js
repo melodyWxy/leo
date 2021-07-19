27ceccff121e080c733fbc8ef05814da0dd4d202
melodyls <17611203170@163.com>
2021-07-19 11:45:37 +0800

    feat: +检索提示

--- index.js
+++ index.js
@@ -19,8 +19,13 @@ class Leo {
     program.command("init")
         .description('初始化一个项目模板')
         .action(async ()=>{
+          const spinner0 = ora('正在检索中，请等待...');
+          let spinner;
           try {
+            spinner0.start();
             const branchs = await getTemplates();
+            spinner0.stop();
+            console.log(chalk.green(`初始化完毕，请自主修改项目目录名以及对应的package.json信息。`));
             const searchRes = await inquirer.prompt([{
               type: 'list',
               message: '请选择模板:',
@@ -28,7 +33,7 @@ class Leo {
               choices: branchs,
             }])
             console.log(`您选择了模板: ${chalk.green(searchRes.template)}`);
-            const spinner = ora('正在初始化中，请等待...');
+            spinner = ora('正在初始化中，请等待...');
             spinner.start();
             await clone(searchRes.template);
             spinner.stop();
@@ -36,6 +41,8 @@ class Leo {
             await deleteRemote();
             process.exit();
           }catch (error) {
+            spinner0.stop();
+            spinner && spinner.stop();
             console.error(error);
             console.error(chalk.yellow('获取远端模板列表失败，请检测网络环境是否友好。'));
             process.exit();
@@ -46,12 +53,17 @@ class Leo {
     program.command("list")
         .description( "查看所有的项目模板")
         .action(async ()=>{
+          const spinner = ora('正在检索中，请等待...');
+          spinner.start();
           try {
             const branchs = await getTemplates();
             await deleteRemote();
+            spinner.stop();
+            console.log(chalk.green('检索成功!'))
             logList(branchs);
             process.exit();
           } catch (error) {
+            spinner.stop();
             console.log(error)
             console.error('获取远端模板列表失败，请检测网络环境是否友好。');
             process.exit();
